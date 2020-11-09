b306973c5a44c3e53501d8d66b90856515f431e6
Braedy Pribelsky <bc.sturdy@live.com>
2020-11-09 16:01:34 -0700

    Update

--- Superhero/hero/models.py
+++ Superhero/hero/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-class Superhero(models.Model):
+class SuperHero(models.Model):
     
     name = models.CharField(max_length=20)
     identity = models.CharField(max_length=20)
