95c2b8585390bad71a1ce8090e7b90d3e4513577
Braedy Pribelsky <bc.sturdy@live.com>
2020-11-16 15:35:12 -0700

    static files

--- SuperHero/SuperHero/settings.py
+++ SuperHero/SuperHero/settings.py
@@ -120,4 +120,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/home/bcsturdy/bacs350/SuperHero/static'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = ['static']
